d788f4351fd2b32b2da4105a9eab43f853a13b95
martinrambo <dieg.nunezt@duocuc.cl>
2023-12-14 11:39:30 -0300

    primer comit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'EasyRide2',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
